fa0f0ae76bacdfde91c855c38e5d7c9bd0558eec
Function <67085909+Function1790@users.noreply.github.com>
2022-08-05 14:44:33 +0900

    Mission15

---
+++ app/missions/mission15/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Hello from './component/Hello';
+
+function App() {
+  return (
+    <div>
+      <div class="one-line">
+        <div id="ONAIR"/>
+        <div id="onair-text">ONLINE</div>
+      </div>
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
